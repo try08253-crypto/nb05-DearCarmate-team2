5e67f7656a2b913f6d5c5165f87e7db09da063e7
918-jihye <wlgp181004@gmail.com>
2025-11-12 17:24:08 +0900

    [Feat] car엔티티 구현, info car 에러부분 추가

--- src/4_shared/exceptions/business.exceptions/exception-info.ts
+++ src/4_shared/exceptions/business.exceptions/exception-info.ts
@@ -1,6 +1,8 @@
 export enum BusinessExceptionType {
   EMPLOYEENUMBER_TOO_LONG,
-
+  INVALID_CAR_NUMBER,
+  INVALID_MILEAGE,
+  INVALID_PRICE,
 }
 
 export const BusinessExceptionTable: Record<BusinessExceptionType, {statusCode: number, message: string}> = {
@@ -8,4 +10,16 @@ export const BusinessExceptionTable: Record<BusinessExceptionType, {statusCode: 
     statusCode: 404,
     message: "비밀번호가 너무 깁니다.",
   },
+  [BusinessExceptionType.INVALID_CAR_NUMBER]: {
+    statusCode: 404,
+    message: "존재하지 않는 차량번호입니다."
+  },
+  [BusinessExceptionType.INVALID_MILEAGE]: {
+    statusCode: 404,
+    message: "입력하신 주행 거리가 비정상적입니다."
+  },
+  [BusinessExceptionType.INVALID_PRICE]: {
+    statusCode: 404,
+    message: "가격은 0이상이어야 합니다."
+  }
 }
